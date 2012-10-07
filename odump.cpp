84440fe123bcf267ec52acb2702411310b33177e
umika <9umika@gmail.com>
2012-10-07 16:13:17 +0900

    add .gitignore and skeleton source odump.cpp

---
+++ odump.cpp
@@ -0,0 +1,19 @@
+/*
+  odump.cpp
+
+  > cl odump.cpp -EHsc -I. -link -LIBPATH:.
+*/
+
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdarg>
+#include <cstdlib>
+#include <cstdio>
+
+int main(int ac, char **av)
+{
+  std::cout << "test" << std::endl;
+  return 0;
+}
